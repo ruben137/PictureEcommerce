eca688bfd91357f750588d8a4772f583407a080f
Ruben Gonzalez <ruben1dario1999@gmail.com>
2021-06-30 17:00:00 -0430

    first commit

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(
   cors({
-    origin: "https://ruben137.github.io",
+    origin: "http://localhost:3000",
     methods: ["GET", "POST", "DELETE", "PATCH", "PUT"],
   })
 );
